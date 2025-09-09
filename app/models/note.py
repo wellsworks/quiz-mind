32b967bca3b7dbde3af0fee759e1b2e21c54b85c
John Wells <johnbwells96@gmail.com>
2025-09-09 08:06:41 -0700

    added models for user and note

---
+++ app/models/note.py
@@ -0,0 +1,20 @@
+# note.py model
+
+from sqlalchemy import Column, Integer, String, ForeignKey, Text
+from app.db import Base
+from sqlalchemy.orm import relationship
+from datetime import datetime, timezone
+
+current_timestamp = datetime.now(timezone.utc)
+
+class Note(Base):
+    __tablename__ = "notes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True, nullable=False)
+    content = Column(Text, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    created_at = Column(String, nullable=False, default=current_timestamp)
+
+    # Relationships
+    user = relationship("User", back_populates="notes")
---
+++ app/models/user.py
@@ -0,0 +1,19 @@
+# user.py model
+
+from sqlalchemy import Column, Integer, String
+from app.db import Base
+from sqlalchemy.orm import relationship
+from datetime import datetime, timezone
+
+current_timestamp = datetime.now(timezone.utc)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    created_at = Column(String, nullable=False, default=current_timestamp)
+
+    # Relationships
+    notes = relationship("Note", back_populates="user", cascade="all, delete-orphan")
