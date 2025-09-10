1a17405ac491d5397220d61c827dd45f1110e7c9
John Wells <johnbwells96@gmail.com>
2025-09-09 22:01:31 -0700

    applied alembic migration for new models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,4 +3,5 @@
 from app.models.user import User
 from app.models.note import Note
 from app.models.flashcard import Flashcard
-
+from app.models.study_sessions import StudySession
+from app.models.user_flashcard_progress import UserFlashcardProgress
