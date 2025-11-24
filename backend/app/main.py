2f50e82211ed9571291b5f1a54b4b4633f02a697
John Wells <johnbwells96@gmail.com>
2025-11-23 17:21:05 -0800

    Added CORS middleware to backend/app/main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,10 +6,26 @@ from app.db import get_db
 from sqlalchemy import text
 from app.routers import user, note, flashcard, study_sessions, auth
 import logging
+import os
+from fastapi.middleware.cors import CORSMiddleware
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = FastAPI()
+
+# Get origins from environment variable, split by comma, and strip whitespace
+# Default to an empty list if the environement variable isn't set
+allowed_origins_str = os.getenv("ALLOWED_CORS_ORIGINS", "")
+origins = [origin.strip() for origin in allowed_origins_str.split(',') if origin.strip()]
+
+# add middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include routers
 app.include_router(user.router)
