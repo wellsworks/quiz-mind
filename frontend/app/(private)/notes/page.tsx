30b1214a39e83e68ebeb67f3a35f3ea9bd6839a2
John Wells <johnbwells96@gmail.com>
2025-12-13 16:43:46 -0800

    Fixed typo in Container component param, updated /notes/page.tsx to add PageHeader, Section, and Container components

--- frontend/app/(private)/notes/page.tsx
+++ frontend/app/(private)/notes/page.tsx
@@ -3,6 +3,9 @@ import  NoteCreateForm  from "@/components/NoteCreateForm";
 import  getCurrentUserSSR  from "@/lib/server-auth";
 import { redirect } from "next/navigation";
 import { getNotesServer } from "@/lib/api-server";
+import PageHeader from "@/components/PageHeader";
+import Section from "@/components/Section";
+import Container from "@/components/Container";
 
 export default async function NotesPage({ params }: { params: { id: string }}) {
     const user = await getCurrentUserSSR();
@@ -13,18 +16,24 @@ export default async function NotesPage({ params }: { params: { id: string }}) {
     const notes = await getNotesServer();
 
     return (
-        <main className="p-6 space-y-8">
-            <h1 className="text-3xl font-bold">Your Notes</h1>
-            
-            <section className="max-w-lg">
-                <NoteCreateForm />
-            </section>
+        <Container className="py-10 space-y-8">
+            <PageHeader 
+                title="Your Notes"
+                description="Upload, organize, and review notes to learn faster."
+            />
 
-            <section className="mt-8">
-                <NotesList />
-            </section>
-            
-        </main>
+            <Section title={"Add a New Note"}>
+                <div className="flex items-center justify-between">
+                    <NoteCreateForm />
+                </div>
+            </Section>      
+
+            <Section title={"All Notes"}>
+                <div className="flex-items-center justify-betwen">
+                    <NotesList />
+                </div>
+            </Section>      
+        </Container>
     );
 
 }
