07989f8cb396670ae569799efa255e59652159cc
John Wells <johnbwells96@gmail.com>
2025-12-19 20:36:47 -0800

    updated util/flashcard.py to accept FlashcardCreate schema as a parameter

--- backend/app/util/flashcards.py
+++ backend/app/util/flashcards.py
@@ -1,20 +1,14 @@
 from sqlalchemy.orm import Session
 from app.models.flashcard import Flashcard
-from app.schemas.flashcard import FlashcardOut
+from app.schemas.flashcard import FlashcardCreate
 
 def create_flashcard(
         db: Session, 
         *,
-        user_id: int,
-        note_id: int,
-        question: str,
-        answer: str,
+        data: FlashcardCreate,
 ):
     flashcard = Flashcard(
-        user_id=user_id,
-        note_id=note_id,
-        question=question,
-        answer=answer,
+        **data.model_dump(),
     )
     db.add(flashcard)
     db.commit()
