254b895907f340255baefe6e3a98dd2c67d4c24a
John Wells <johnbwells96@gmail.com>
2025-12-23 18:02:25 -0800

    initialized shadcn in /frontend

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { ReactQueryProvider } from "@/components/ReactQueryProvider";
 import ThemeHydration from "@/components/ThemeHydration";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
