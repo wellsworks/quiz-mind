1637d5161d1ebc797ec9b03bb88c921d704c25ec
John Wells <johnbwells96@gmail.com>
2026-01-01 10:26:46 -0800

    update layout and spacing of /app/login/page.tsx and add link back to app/page.tsx

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,11 +1,23 @@
 "use client";
 
 import LoginForm from "@/components/LoginForm";
+import Link from "next/link";
 
 export default function LoginPage() {
     return (
-        <div className="flex justify-center items-center min-h-screen p-4">
-            <LoginForm />
+        <div className="grid min-h-svh">
+            <div className="flex flex-col gap-4 p-6 md:p-10">
+                <div className="flex justify-center gap-2 md:justify-start">
+                    <Link href="/" className="flex items-center gap-2 font-medium">
+                        QuizMind
+                    </Link>
+                </div>
+                <div className="flex flex-1 items-center justify-center">
+                    <div className="w-full max-w-xs">
+                        <LoginForm />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
