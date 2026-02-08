7974fd3255a327f5b927873995c0964a2ab3b034
John Wells <johnbwells96@gmail.com>
2026-02-07 21:34:57 -0800

    update notes/page.tsx to remove getCurrentUserSSR and getNotesServer function calls

--- frontend/app/(private)/notes/page.tsx
+++ frontend/app/(private)/notes/page.tsx
@@ -1,20 +1,11 @@
 import NotesList from "@/components/NotesList";
 import  NoteCreateForm  from "@/components/NoteCreateForm";
-import  getCurrentUserSSR  from "@/lib/server-auth";
-import { redirect } from "next/navigation";
-import { getNotesServer } from "@/lib/api-server";
 import PageHeader from "@/components/PageHeader";
 import Section from "@/components/Section";
 import Container from "@/components/Container";
 import { Separator } from "@/components/ui/separator";
 
 export default async function NotesPage({ params }: { params: { id: string }}) {
-    const user = await getCurrentUserSSR();
-    const noteId  = params.id
-    
-    if (!user) redirect("/login");
-
-    const notes = await getNotesServer();
 
     return (
         <Container className="py-10 space-y-6 bg-background text-foreground">
