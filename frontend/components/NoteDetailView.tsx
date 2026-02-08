3604554e1bfde307228bc97df1ef3139147fa040
John Wells <johnbwells96@gmail.com>
2026-02-07 21:19:26 -0800

    update NoteDetailView component props to take noteId and call useNoteById hook, as well as add skeleton components

--- frontend/components/NoteDetailView.tsx
+++ frontend/components/NoteDetailView.tsx
@@ -1,12 +1,32 @@
 "use client";
-
+import { useNoteById } from "@/lib/hooks/notes";
+import { Skeleton } from "./ui/skeleton";
 import Container from "@/components/Container";
 
-export default function NoteDetailView({ 
-    note, 
-}: { 
-    note: { id: number; title: string; content: string }; 
-}) {
+export default function NoteDetailView({ noteId }: { noteId: string }) {
+    const { data, isLoading, isError } = useNoteById(noteId);
+    const note = data;
+
+    if (isLoading) {
+        return (
+            <div className="flex flex-col space-y-2">
+                <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+                <div className="space-y-2">
+                    <Skeleton className="h-4 w-[250px]" />
+                    <Skeleton className="h-4 w-[200px]" />
+                </div>
+            </div>
+        );
+    }
+
+    if (isError || !note) {
+        return (
+            <div className="text-red-500 text-sm">
+                Something went wrong. Try refreshing the page.
+            </div>
+        );
+    }
+
 
     return (
         <Container className="w-full space-y-6 bg-background text-foreground">
