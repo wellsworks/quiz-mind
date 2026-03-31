1489256342133f705aa14c2f91f1d3cfbdfaf1b7
John Wells <johnbwells96@gmail.com>
2026-03-31 12:48:41 -0700

    update better-auth instance to connect to postgres database

--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -1,8 +1,40 @@
 import { betterAuth } from "better-auth";
+import { Pool } from "pg";
+
+export const backend = process.env.BACKEND_URL || "";
+export const DB_URL = process.env.BETTER_AUTH_DB;
 
 export const auth = betterAuth({
+    database: new Pool({
+        connectionString: DB_URL,
+    }),
     emailAndPassword: {
         enabled: true,
     },
+    session: {
+        cookieCache: {
+            enabled: true,
+            maxAge: 60 * 60,
+            strategy: "jwt",
+            secret: process.env.SECRET_KEY,
+            refreshCache: {
+                updateAge: 60
+            }
+        },
+    },
+    account: {
+        storeStateStrategy: "cookie",
+        storeAccountCookie: true,
+    },
+    advanced: {
+        defaultCookieAttributes: {
+            sameSite: "none",
+            secure: true,
+            httpOnly: true,
+        }
+    },
+    trustedOrigins: [
+        backend,
+    ],
 });
 
