f73034895b43d7ad4c2a168019f74c174a947465
John Wells <johnbwells96@gmail.com>
2026-06-17 13:13:05 -0700

    add types for Container component and Private Layout props

--- frontend/app/(private)/layout.tsx
+++ frontend/app/(private)/layout.tsx
@@ -1,6 +1,15 @@
 import Navbar from "@/components/Navbar";
 import LayoutWrapper from "@/components/LayoutWrapper";
+import { ReactNode } from "react";
 
-export default function PrivateLayout({ children }) {
-    return <LayoutWrapper navbar={<Navbar /> }>{children}</LayoutWrapper>
+export default function PrivateLayout({ 
+    children,
+ }: {
+    children: ReactNode;
+ }) {
+    return (
+        <LayoutWrapper navbar={<Navbar /> }>
+            {children}
+        </LayoutWrapper>
+    );
 }
--- frontend/components/Container.tsx
+++ frontend/components/Container.tsx
@@ -1,3 +1,15 @@
-export default function Container({ children, className = "" }) {
-    return <div className={`container mx-auto px-4 ${className}`}>{children}</div>;
+import { ReactNode } from "react";
+
+export default function Container({ 
+    children, 
+    className,
+}: {
+    children: ReactNode;
+    className: string;
+}) {
+    return (
+        <div className={`container mx-auto px-4 ${className}`}>
+            {children}
+        </div>
+    );
 }
