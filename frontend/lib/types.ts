cd93c361091b949302a4678a7dca4789037b67d6
John Wells <johnbwells96@gmail.com>
2026-06-18 12:32:45 -0700

    add Note and Flashcard interfaces in types.ts

--- frontend/lib/types.ts
+++ frontend/lib/types.ts
@@ -2,3 +2,17 @@ export interface LoginResponse {
     access_token: string;
     token_type: string;
 }
+
+export interface Note {
+    id: number;
+    title: string;
+    content: string;
+}
+
+export interface Flashcard {
+    id: number;
+    question: string; 
+    answer: string; 
+    note_id: number; 
+    source: string
+}
